e99bf9b404c5e3dc8ac805fc95a402aea9ed50ed
Nuni <nuni@kovrr.com>
2020-08-26 17:03:27 +0300

    add-typing-and-black

--- chancepy/Chance.py
+++ chancepy/Chance.py
@@ -1,46 +1,48 @@
 import random
 import string
+from typing import Optional
 import uuid
+
+MAX_INT = 9007199254740991
+MIN_INT = -MAX_INT
 
 
 class Chance:
-    def guid(self):
+    def guid(self) -> str:
         return str(uuid.uuid4())
 
-    def boolean(self, likelihood: float = 50):
+    def boolean(self, likelihood: float = 50) -> bool:
         if random.random() * 100 > likelihood:
             return True
         return False
 
-    def character(self, pool=None):
+    def character(self, pool: Optional[str] = None) -> str:
         randpool = pool
         if not randpool:
-            randpool = string.ascii_letters + '!@#$%^&*()1234567890'
+            randpool = string.ascii_letters + "!@#$%^&*()1234567890"
         return random.choice(randpool)
-    
-    def letter(self, casing='lower'):
-        if casing == 'lower':
-            pool = string.ascii_lowercase
-        elif casing == 'upper':
-            pool = string.ascii_uppercase
-        else:
-            raise ValueError('Invalid casing. choose "lower" or "upper"')
-        return self.character(pool=pool)
 
-    def string(self, length=None, pool=None):
+    def letter(self, casing: str = "lower") -> str:
+        pools = {
+            "lower": string.ascii_lowercase,
+            "upper": string.ascii_uppercase,
+        }
+        if casing not in pools.keys():
+            raise ValueError('Invalid casing. choose "lower" or "upper"')
+        return self.character(pool=pools[casing])
+
+    def string(self, length: Optional[int] = None, pool: Optional[str] = None) -> str:
         if not length:
             length = self.natural(5, 20)
-        
-        return ''.join(self.character(pool=pool) for _ in range(length))
 
-    def floating(self, mini=0, maxi=100):
+        return "".join(self.character(pool=pool) for _ in range(length))
+
+    def floating(self, mini: float = 0, maxi: float = 100) -> float:
         return random.uniform(mini, maxi)
-    
-    def integer(self, mini=-9007199254740991, maxi=9007199254740991):
+
+    def integer(self, mini: int = -MIN_INT, maxi: int = MAX_INT) -> int:
         return random.randint(mini, maxi)
-    
-    def natural(self, mini=0, maxi=9007199254740991):
+
+    def natural(self, mini: int = 0, maxi: int = MAX_INT) -> int:
         mini = max(0, mini)
         return random.randint(mini, maxi)
-
-
--- chancepy/__init__.py
+++ chancepy/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '0.1.0'
+from .Chance import Chance
+
+__version__ = "0.1.0"
+__all__ = [Chance]
