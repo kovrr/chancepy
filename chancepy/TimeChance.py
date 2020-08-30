fcd3ce6ee2dd8aac5f8ccad44a285678c6f0c1f1
Nuni <nuni@kovrr.com>
2020-08-30 23:56:58 +0300

    finish-time

--- chancepy/TimeChance.py
+++ chancepy/TimeChance.py
@@ -1,3 +1,4 @@
+import pytz
 from .BaseRandom import BaseRandom
 import datetime
 
@@ -30,3 +31,51 @@ class TimeChance(BaseRandom):
         now = int(datetime.datetime.now().timestamp())
         random_timestamp_until_now = cls.random.randint(0, now)
         return random_timestamp_until_now
+
+    @classmethod
+    def year(cls, mini=None, maxi=None):
+        if not mini:
+            mini = datetime.date.today().year
+        return cls.random.randint(mini, maxi if maxi else mini + 100)
+
+    @classmethod
+    def month(cls, mode="numeric"):
+        month = cls.random.randint(1, 12)
+        if mode == "numeric":
+            return month
+        if mode == "short":
+            return datetime.date(1970, month, 0).strftime("%b")
+        if mode == "full":
+            return datetime.date(1970, month, 1).strftime("%B")
+        raise ValueError("invalid mode, choose 'numeric', 'short' or 'full")
+
+    @classmethod
+    def weekday(cls, mode="full"):
+        date = cls.date()
+        if mode == "full":
+            return date.strftime("%A")
+        if mode == "short":
+            return date.strftime("%a")
+        if mode == "numeric":
+            return date.weekday()
+        raise ValueError("invalid mode, choose 'numeric', 'short' or 'full")
+
+    @classmethod
+    def hour(cls, twentyfour=False):
+        return cls.random.randint(1, 24 if twentyfour else 12)
+
+    @classmethod
+    def minute(cls):
+        return cls.random.randint(0, 59)
+
+    @classmethod
+    def second(cls):
+        return cls.random.randint(0, 59)
+
+    @classmethod
+    def millisecond(cls):
+        return cls.random.randint(0, 999)
+
+    @classmethod
+    def timezone(cls):
+        return pytz.timezone(cls.random.choice(pytz.all_timezones))
